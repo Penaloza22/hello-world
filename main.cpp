038cab78b937ae472d4dccff65f912d9525c2d7e
Armando Penaloza <apenaloza@uky.edu>
2025-04-02 15:54:22 -0400

    Ask and greet user

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-
+#include <string>
 int main(){
-  std::cout << "Hellow World." << std::endl;
+
+    std::string full_name;
+
+    //asking user's name and storing it in the variable "full_name"
+    std::cout << "Hi there! Enter your name:\n";
+    std::getline(std::cin, full_name);
+
+  std::cout << "Hello " << full_name << "." << std::endl;
   return 0;
 }
